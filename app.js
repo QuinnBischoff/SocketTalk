f67bbe9124d3f83ba19456c9ed0670d5b6a1a4a1
Quinn Bischoff <quinn.qb@gmail.com>
2019-02-19 20:07:14 -0700

    Basic Socket.io chat app

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
+var path = require('path');
+var io = require('socket.io')(http);
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/public/index.html');
 });
 
+io.on('connection', function(socket){
+  console.log('a user connected');
+  
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+  
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+  });
+});
+
+
 http.listen(3000, function(){
-  console.log('listening on *:3000');
+  console.log('listening on port 3000');
 });
