676fa85cd874fd4f361931c2e9ddd354af2c0b78
Quinn Bischoff <quinn.qb@gmail.com>
2019-03-01 03:22:24 -0700

    working properly

--- app.js
+++ app.js
@@ -1,40 +1,117 @@
 var express = require('express');
+var cookieReader = require('cookie');
 var app = express();
 var http = require('http').Server(app);
 var path = require('path');
 var io = require('socket.io')(http);
 
 let messages = [];
-let usernames = [];
+let usernames = ['Koala', 'Elephant', 'Kangaroo', 'Dolphin', 'Grizzly', 'Moose'];
+let users = [];
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/public/index.html');
+//  console.log("here");
+ // res.cookie("username", "quinn");
+ // res.sendFile(__dirname + '/public/index.html');
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.emit('message load', JSON.stringify(messages));
+  let cookieString = socket.handshake.headers.cookie;
+  console.log(cookieString);
+  let connectedUser;
+  let newUser = true;
   
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
+  if (cookieString) {
+    let cookies = cookieReader.parse(socket.handshake.headers.cookie);
+    console.log(cookies);
+    //create a new user object
+    if (cookies.username) {
+      connectedUser = users.find(user => user.name === cookies.username);
+      if (connectedUser) {
+        newUser = false;
+        connectedUser.active = true;
+      }
+    }
+  }
+  if (newUser) {
+    connectedUser = {
+      name: getRandomName(),
+      color: '000000',
+      active: true,
+      };
+    users.unshift(connectedUser); //add user to users list
+  }
+  socket.user= connectedUser; //set the user for the socket so we know who the socket is
+  socket.emit('message load', 
+    JSON.stringify(
+      {
+       username: connectedUser.name,
+       messages: messages,
+    })); //send the user their set name, and all the existing messages
+  //tell everyone to update their active list
+  io.emit('active user update', getActiveUsers());
+  
+
+  socket.on('disconnect', function() {
+    users[users.indexOf(socket.user)].active = false; //remove from active
+    //tell everyone to update their active list
+    io.emit('active user update', getActiveUsers());
   });
   
   socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
-
     let timeStamp = new Date().toLocaleTimeString(undefined, {
       hour: '2-digit',
       minute: '2-digit',
     });
-
-    messageItem = {time: timeStamp, message: msg};
+    messageItem = {time: timeStamp, message: msg, sender: socket.user.name, color: socket.user.color};
     messages.unshift(messageItem);
     io.emit('chat message', messageItem);
   });
+
+  socket.on('color change', function(data) {
+    socket.user.color = data.newColor;
+  });
+
+  socket.on('name change', function(data) {
+    if (!validateNewName(data.newName)) {
+      return;
+    }
+    socket.user.name = data.newName;
+    socket.emit('change name', data.newName);
+    io.emit('active user update', getActiveUsers());
+  });
+
 });
 
 http.listen(3000, function(){
   console.log('listening on port 3000');
 });
+
+//Helper functions
+function getActiveUsers() {
+  let activeUsers = users.filter(user => {
+    return user.active === true
+  });
+  return activeUsers;
+}
+
+function validateNewName(name) {
+  for (user of users) {
+    if (user.name == name) {
+      return false;
+    }
+  }
+  return true;
+}
+//gets a random name, if another user already has it, then try another.
+function getRandomName() {
+  let nameIndex = Math.floor(Math.random()*usernames.length);
+  let name = usernames[nameIndex];
+  while (users.find(a => a.name == name)) {
+    nameIndex = Math.floor(Math.random()*usernames.length);
+    name = usernames[nameIndex];
+  }
+  return name;
+}
--- public/scripts.js
+++ public/scripts.js
@@ -1,38 +1,120 @@
 $(function() {
-  var socket = io();
+  let socket = io();
   scrollDown();
 
+  // Form is sent handler
   $("form").submit(function(e) {
     e.preventDefault(); // prevents page reloading
-    socket.emit("chat message", $("#m").val());
-    $("#m").val("");
+    let input = $("#input-form").val();
+    
+    if (input.startsWith('/nickcolor')) {
+      let color = input.substring(input.indexOf('r')+ 1, input.length );
+      color = color.trim();
+
+      if (color && /(^[0-9A-F]{6}$)/i.test(color)) {
+        socket.emit("color change",
+        {
+          newColor: color
+        });
+      }
+    }
+    else if (input.startsWith('/nick')) {
+      let nickname = input.substring(input.indexOf('k')+ 1, input.length );
+      nickname = nickname.trim();
+      if (nickname) {
+        socket.emit("name change", 
+        {
+         newName: nickname
+        });
+      }
+    }
+    else {
+      socket.emit("chat message",  input);
+    }
+    $("#input-form").val("");
     return false;
   });
 
   //SOCKET INPUT HANDLERS
-  socket.on('chat message', function(msgItem){
-    appendMessageToList(msgItem);
+  socket.on('chat message', function(msg){
+    appendMessageToList(msg);
     scrollDown();
   });
 
-  socket.on('message load', function(msgs) {
-    let messages = JSON.parse(msgs);
-    console.log(messages);
-    messages = messages.reverse();
+  socket.on('change name', function(newName) {
+    setUserName(newName);
+  });
+
+  socket.on('message load', function(load) {
+    let msgLoad = JSON.parse(load);
+    // set username
+    setUserName(msgLoad.username);
+    // fill out messages
+    messages = msgLoad.messages.reverse();
     for (msg in messages) {
       appendMessageToList(messages[msg]);
     }
     scrollDown();
   });
 
+  socket.on('active user update', function(activeUsers) {
+    updateActiverUserList(activeUsers); //add to list on screen
+  });
+
   //HELPER FUNCTIONS
-  function appendMessageToList(msgItem) {
-    $('#message-list').prepend($('<li>').text(msgItem.time + ": " + msgItem.message));
+  function appendMessageToList(msg) {
+    let sentFrom = msg.sender;
+    //if it's from us, bold it
+    if (sentFrom == getUserName()) {
+      sentFrom = '<b>'+ sentFrom +'</b>';
+    }
+    //add color as class
+    sentFrom = '<span class=' + msg.color  +'>' + sentFrom + '</span>';
+    $('#message-list').append($('<li>').innerHTML = ("<p>" + msg.time + " " + sentFrom + ": " + msg.message + "</p>"));
+    //style items with that class, with that specific color
+    $('.' + msg.color).css({'color': '#' + msg.color});
+    scrollDown();
+  }
+
+  function appendActiveUserToList(user) {
+    $('#users-list').prepend($('<li id=user-' + user.name + '>').text(user.name));
+  }
+
+  function updateActiverUserList(users) {
+    $('#users-list').empty();
+    for(user of users) {
+      appendActiveUserToList(user);
+    }
+  }
+
+  function setUserName(name) {
+    $('#username').text(name);
+    document.cookie = 'username=' + name;
+  }
+
+  function getUserName() {
+    return $('#username').text();
   }
 
   function scrollDown(){
-    console.log("scroll");
-    $('#message-box').scrollTop($('#message-box')[0].scrollHeight);
+    $('#message-list').animate({scrollTop: $('#message-list').prop("scrollHeight")}, 1);
   }
+
+  function getCookie(cookieName) {
+    var name = cookieName + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+
 });
 
